c8f8ba88f7d758c20c23421f537f89c73b327779
aufarabul <aufarabul@gmail.com>
2024-04-26 19:42:27 +0700

    cors

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fileUpload = require("express-fileupload");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(
   fileUpload({
